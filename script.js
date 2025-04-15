ca51242995554149cfbfae65957062ff10b31eac
GoblinK1ng <169468964+GoblinK1ng@users.noreply.github.com>
2025-04-15 17:25:33 +0000

    Feature: Game ends when 3 X's are in a row

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function createPlayer (name){
-    const points = 0;
+    let points = 0;
 
     const givePoint = () => points++;
-    return {name, points, givePoint};
+    return {name, givePoint};
 }
 
 
@@ -10,26 +10,50 @@ function game(){
     const playerOne = createPlayer("Jack");
     const playerTwo = createPlayer("Meghan");
 
+    playerOne.givePoint();
+
     const gameBoard = createGameboard();
 
-    while (true){
+    let gameLoop = true;
+    while (gameLoop){
         const playerChoice = [prompt("X"), prompt("Y")];
 
         gameBoard.addSymbol("X", playerChoice[0], playerChoice[1]);
         
-        console.table(gameBoard.gameBoard);
+        console.table(gameBoard.board);
+        
+        let countX = 0;
+        let countO = 0;
+        for (let x in gameBoard.board){
+            for (let i in gameBoard.board[x]){
+                if (gameBoard.board[x][i] === "X"){
+                    countX++;
+                    console.log(countX);
+                }
+                else if (gameBoard.board[x][i] === "O"){
+                    countO++;
+                    console.log(countO);
+                }
+                
+                if (countX === 3){
+                    gameLoop = false;
+                }
+            }
+            countX = 0;
+            countO = 0;
+        }
     }
 
 }
 
 function createGameboard(){
-    const gameBoard = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+    const board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
     const addSymbol = (symbol, positionX, positionY) => {
         console.log(positionX, positionY);
-        gameBoard[positionX][positionY] = symbol;
+        board[positionX][positionY] = symbol;
         
     }
-    return {gameBoard, addSymbol};
+    return {board, addSymbol};
 
 
 
