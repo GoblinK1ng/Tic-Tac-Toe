1d4e9b62a4aed798ff868c223b5e0f1d9e1f7052
GoblinK1ng <169468964+GoblinK1ng@users.noreply.github.com>
2025-04-15 16:34:04 +0000

    Added skeleton for main objects for game

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function createPlayer (name){
     return {name};
 }
+
+const playerOne = createPlayer("Jack");
+console.log(playerOne.name);
+
+function game(){
+
+}
+
+function gameBoard(){
+    
+}
