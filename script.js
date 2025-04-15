dad2ac1f4c693dc3b77491e0500e5ce19055f4fe
GoblinK1ng <169468964+GoblinK1ng@users.noreply.github.com>
2025-04-15 16:40:10 +0000

    Added gameboard array, to store player moves, as well as generic player creation

--- script.js
+++ script.js
@@ -6,9 +6,16 @@ const playerOne = createPlayer("Jack");
 console.log(playerOne.name);
 
 function game(){
+    const playerOne = createPlayer("Jack");
+    const playerTwo = createPlayer("Meghan");
 
+    gameBoard();
 }
 
 function gameBoard(){
-    
+    const gameBoard = Array(3).fill(Array(3).fill(0));
+
+    console.table(gameBoard);
 }
+
+game();
