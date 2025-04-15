c020d2150c2d793f4e70eb9de43bdd41114191e4
GoblinK1ng <169468964+GoblinK1ng@users.noreply.github.com>
2025-04-15 16:48:05 +0000

    Added givePoint function to player to add points if they win, as well as prompt for player Choice

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 function createPlayer (name){
-    return {name};
+    const points = 0;
+
+    const givePoint = () => points++;
+    return {name, points, givePoint};
 }
 
-const playerOne = createPlayer("Jack");
-console.log(playerOne.name);
 
 function game(){
     const playerOne = createPlayer("Jack");
     const playerTwo = createPlayer("Meghan");
 
+    function gameBoard(){
+        const gameBoard = Array(3).fill(Array(3).fill(0));
+    
+        console.table(gameBoard);
+    }
+
+    const playerChoice = prompt();
     gameBoard();
+
 }
 
-function gameBoard(){
-    const gameBoard = Array(3).fill(Array(3).fill(0));
 
-    console.table(gameBoard);
-}
 
 game();
