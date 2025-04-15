f81c051c8ba5fc2ca49b90fd507443211800eb6e
GoblinK1ng <169468964+GoblinK1ng@users.noreply.github.com>
2025-04-15 19:45:50 +0000

    Organized code, removed test/debugging code

--- script.js
+++ script.js
@@ -2,15 +2,17 @@ function createPlayer (name){
     let points = 0;
 
     const givePoint = () => points++;
-    return {name, givePoint};
+    const Winner = () => {
+        console.log(name + " is the Winner, gains one point");
+        
+    }
+    return {name, givePoint, Winner};
 }
 
 
 function game(){
     const playerOne = createPlayer("Jack");
     const playerTwo = createPlayer("Meghan");
-
-    playerOne.givePoint();
 
     const gameBoard = createGameboard();
 
@@ -32,54 +34,55 @@ function game(){
         
         console.table(gameBoard.board);
         
-        let countX = 0;
-        let countO = 0;
 
-        const check = checkWinner(gameBoard.board);
+        const check = gameBoard.checkWinner();
+
         if (check === "X"){
+            playerOne.Winner();
             playerOne.givePoint();
             gameLoop = false;
-            console.log(playerOne.name);
         }
         else if (check === "O"){
+            playerTwo.Winner();
             playerTwo.givePoint();
             gameLoop = false;
-            console.log(playerOne.name);
         }
         
     }
 
 }
 
-function checkWinner(board){
-    if (board[1][0] === board[1][1] && board[1][0] === board[1][2] ||
-        board[0][1] === board[1][1] && board[0][1] === board[2][1]){
-        return board[1][1];
-    }
 
-    else if (board[0][0] === board[0][1] && board[0][0] === board[0][2] ||
-        board[0][0] === board[1][0] && board[0][0] === board[2][0] ||
-        board[0][0] === board[1][1] && board[0][0] === board[2][2]){
-        return board[0][0];
-    }
-
-    else if (board[0][2] === board[1][2] && board[0][2] === board[2][2] ||
-        board[0][2] === board[1][1] && board[0][2] === board[2][0]){
-        return board[0][2];
-    }
-    else if (board[2][0] === board[2][1] && board[2][0] === board[2][2]){
-        return board[2][0];
-    }
-}
 
 function createGameboard(){
     const board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
     const addSymbol = (symbol, positionX, positionY) => {
-        console.log(positionX, positionY);
+
         board[positionX][positionY] = symbol;
         
     }
-    return {board, addSymbol};
+
+    function checkWinner(){
+        if (board[1][0] === board[1][1] && board[1][0] === board[1][2] ||
+            board[0][1] === board[1][1] && board[0][1] === board[2][1]){
+            return board[1][1];
+        }
+    
+        else if (board[0][0] === board[0][1] && board[0][0] === board[0][2] ||
+            board[0][0] === board[1][0] && board[0][0] === board[2][0] ||
+            board[0][0] === board[1][1] && board[0][0] === board[2][2]){
+            return board[0][0];
+        }
+    
+        else if (board[0][2] === board[1][2] && board[0][2] === board[2][2] ||
+            board[0][2] === board[1][1] && board[0][2] === board[2][0]){
+            return board[0][2];
+        }
+        else if (board[2][0] === board[2][1] && board[2][0] === board[2][2]){
+            return board[2][0];
+        }
+    }
+    return {board, addSymbol,checkWinner};
 
 
 
